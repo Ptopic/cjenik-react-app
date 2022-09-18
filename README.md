00dab9d467c92da7b7c5762d58a9a16b8dc5024b
Ptopic <pingo15102002@gmail.com>
2022-09-19 00:03:19 +0200

    initial commit, with readme and react boiler plate

---
+++ README.md
@@ -0,0 +1,22 @@
+## Cijenik app made with <3 and react
+
+### TODO LIST:
+
+- [ ] npm packages
+
+  - [x] concurrently
+  - [x] framer-motion
+  - [x] react-icons
+  - [x] react-router-dom
+
+- [ ] loop thru PiceData and render it as pice components (cards)
+  - [ ] add image to each pice component
+    - [ ] do same for food
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+- [ ]
---
+++ src/data/PiceData.js
@@ -0,0 +1,206 @@
+const PiceData = [
+	{
+		'topli napitci': {
+			img: 'img',
+			content: {
+				Kava: {
+					price: '12',
+				},
+				'Kava mlijeko ili šlag': {
+					price: '14',
+				},
+				'Kava mlijeko ili šlag velika': {
+					price: '14',
+				},
+				Capuccino: {
+					price: '16',
+				},
+				'Bijela kava': {
+					price: '15',
+				},
+				Nescafe: {
+					price: '15',
+				},
+				Čaj: {
+					price: '12',
+				},
+				Mlijeko: {
+					price: '15',
+				},
+			},
+		},
+		'prirodni sokovi': {
+			img: 'img',
+			content: {
+				Naranća: {
+					price: '19',
+				},
+				Jabuka: {
+					price: '19',
+				},
+				Ribizla: {
+					price: '19',
+				},
+				Gusti: {
+					price: '19',
+				},
+				'Prirodna limunada': {
+					price: '23',
+				},
+				'Prirodna narančada': {
+					price: '28',
+				},
+				'Ledeni čaj': {
+					price: '19',
+				},
+				Cedevita: {
+					price: '17',
+				},
+			},
+		},
+		'gazirani sokovi': {
+			img: 'img',
+			content: {
+				Špeci: {
+					price: '21',
+				},
+				"Pepsi, Miranda, 7'up": {
+					price: '19',
+				},
+				Tonic: {
+					price: '19',
+				},
+				'Bitter lemon': {
+					price: '19',
+				},
+				'Pipi, Nara': {
+					price: '19',
+				},
+				Cocta: {
+					price: '19',
+				},
+				Orangina: {
+					price: '20',
+				},
+				Limona: {
+					price: '20',
+				},
+			},
+		},
+		'mineralna voda': {
+			img: 'img',
+			content: {
+				'Mineralna voda': {
+					price: '12',
+				},
+				'Mineralna senzacija': {
+					price: '17',
+				},
+				'Mineralna voda': {
+					price: '20',
+				},
+				'Prirodna voda': {
+					price: '15',
+				},
+				'Prirodna voda': {
+					price: '22',
+				},
+			},
+		},
+		pivo: {
+			img: 'img',
+			content: {
+				Karlovačko: {
+					price: '17',
+				},
+				'Karlovačko crno': {
+					price: '22',
+				},
+				'Karlovačko radler': {
+					price: '22',
+				},
+				'Točeno pivo (Karlovačko)': {
+					price: '18',
+				},
+				'Točeno pivo (Karlovačko)': {
+					price: '25',
+				},
+				'Točeno pivo (Heineken)': {
+					price: '20',
+				},
+				'Točeno pivo (Heineken)': {
+					price: '30',
+				},
+				Heineken: {
+					price: '22',
+				},
+				'Heineken bezalkoholni': {
+					price: '22',
+				},
+				'Cider Strongbow': {
+					price: '22',
+				},
+				'Krombacher Pils': {
+					price: '20',
+				},
+				'Krombacher Weizen': {
+					price: '30',
+				},
+			},
+		},
+		aperitivi: {
+			img: 'img',
+			content: {
+				Rakija: {
+					price: '16',
+				},
+				Pelinkovac: {
+					price: '20',
+				},
+				Stock: {
+					price: '18',
+				},
+				Amaro: {
+					price: '16',
+				},
+				Vodka: {
+					price: '13',
+				},
+				Gin: {
+					price: '19',
+				},
+				'Orahovac, Brandy': {
+					price: '16',
+				},
+			},
+		},
+		'strana alkoholna pica': {
+			img: 'img',
+			content: {
+				'Johhny Walker': {
+					price: '20',
+				},
+				"Jack Daniel's": {
+					price: '25',
+				},
+				Chivas: {
+					price: '30',
+				},
+				Jagermeister: {
+					price: '20',
+				},
+				Campari: {
+					price: '18',
+				},
+				Martell: {
+					price: '25',
+				},
+				Ballantines: {
+					price: '20',
+				},
+			},
+		},
+	},
+];
+
+export default PiceData;
